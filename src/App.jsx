8cd244e3caad6f9f95956565d130bbeaaa9bdbbe
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-02-25 13:43:12 +0300

    color

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Colors from "./Component/Colors";
+
+function App() {
+  return (
+    <div>
+      <h2 className="text-3xl font-bold underline">
+        I am always in the top !!!
+        <Colors />
+      </h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Colors.jsx
@@ -0,0 +1,17 @@
+function Colors() {
+  return (
+    <div>
+      <h2 className="text-slate-400 ">Iam always in the top !!!</h2>
+      <h2 className="text-red-600">Iam always in the top !!!</h2>
+      <h2 className="text-orange-400">Iam always in the top !!!</h2>
+      <h2 className="text-gray-200">Iam always in the top !!!</h2>
+      <h2 className="text-green-300">Iam always in the top !!!</h2>
+      <h2 className="text-purple-600">Iam always in the top !!!</h2>
+      <h2 className="text-stone-900">Iam always in the top !!!</h2>
+      <h2 className="text-amber-500">Iam always in the top !!!</h2>
+      <h2 className="text-pink-700">Iam always in the top !!!</h2>
+    </div>
+  );
+}
+
+export default Colors;
