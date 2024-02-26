0ea161f5a19f0232fc75490ff8aa1934fcbcec98
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-02-26 14:03:54 +0300

    spaces-sizing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 // import Colors from "./Component/Colors";
 // import Customization from "./Component/Customization";
-
-import Typography from "./Component/Typography";
+// import Typography from "./Component/Typography";
+import SpacesSize from "./Component/SpacesSize";
 
 function App() {
   return (
     <div>
       {/* <Colors /> */}
       {/* <Customization /> */}
-      <Typography />
+      {/* <Typography /> */}
+      <SpacesSize />
     </div>
   );
 }
---
+++ src/Component/SpacesSize.jsx
@@ -0,0 +1,60 @@
+function SpacesSize() {
+  return (
+    <>
+      <div className="w-screen mx-3 my-3 m-3 px-3 py-3  p-3 bg-sky-600 text-white font-bold">
+        <h1 className="font-bold text-pink-400 bg-sky-300">
+          Street Fighter Characters
+        </h1>
+      </div>
+      <div className=" w-1/2 mx-3 my-3 m-3 px-3 py-3  p-3 bg-red-600 text-white font-bold">
+        Chun Lİ
+      </div>
+      <div className=" w-1/3 mx-3 my-3 m-3 px-3 py-3  p-3 bg-orange-600 text-white font-bold">
+        Ryu
+      </div>
+      <div className=" w-1/4 mx-3 my-3 m-3 px-3 py-3  p-3 bg-green-600 text-white font-bold">
+        Ken
+      </div>
+      <div className="w-1/5 mx-3 my-3 m-3 px-3 py-3  p-3 bg-purple-600 text-white font-bold">
+        Blanka
+      </div>
+      <div className="flex flex-row ">
+        <h2 className="h-32 w-1/3 m-4 p-2  bg-black text-white">merhaba</h2>
+        <h2 className="h-32 w-1/3 m-4 p-2 bg-red-600 text-white">merhaba</h2>
+        <h2 className="h-32 w-1/3 m-4 p-2 bg-blue-600 text-white">merhaba</h2>
+      </div>
+    </>
+  );
+}
+
+export default SpacesSize;
+
+/*
+
+p-0	   padding: 0px;
+px-0	 padding-left: 0px , padding-right: 0px;
+py-0	 padding-top: 0px  , padding-bottom: 0px;
+
+pt-0  padding-top 0px
+pb-0  padding-bottom 0px
+pr-0  padding-right 0px
+pl-0  padding-left 0px
+
+w-0	width: 0px;
+w-px	width: 1px;
+w-0.5	width: 0.125rem;  2px 
+w-1	width: 0.25rem;  4px 
+w-1.5	width: 0.375rem;  6px 
+w-2	width: 0.5rem;  8px
+
+
+<div class="flex ...">
+  <div class="w-1/2 ... ">w-1/2</div>
+  <div class="w-1/2 ... ">w-1/2</div>
+</div>
+<div class="flex ...">
+  <div class="w-2/5 ...">w-2/5</div>
+  <div class="w-3/5 ...">w-3/5</div>
+</div>
+
+*/
